791d7c376df025074f447b7bcf733973ce96ee06
Abe_liu <Abe_liu@outlook.com>
2017-11-15 17:46:51 +0800

    Initial Commit

---
+++ FFMpeguse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFMpeguse
+//
+//  Created by Abe_liu on 2017/11/15.
+//  Copyright © 2017年 Abe_liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFMpeguse/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FFMpeguse
+//
+//  Created by Abe_liu on 2017/11/15.
+//  Copyright © 2017年 Abe_liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
