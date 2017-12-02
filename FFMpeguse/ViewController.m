623f8af42104eb3cb1b60790d2b7a870e6eef93e
Abe_liu <Abe_liu@outlook.com>
2017-12-02 12:09:59 +0800

    FFmpeg解码操作
    提交代码

--- FFMpeguse/ViewController.m
+++ FFMpeguse/ViewController.m
@@ -7,9 +7,30 @@
 //
 
 #import "ViewController.h"
+#import <libavcodec/avcodec.h>
+#import <libavformat/avformat.h>
+#import <libavutil/avutil.h>
+#import <libswscale/swscale.h>
+#import <libavutil/imgutils.h>
+#import "OpenglView.h"
+#import "MoviePlayer.h"
 
 @interface ViewController ()
-
+{
+    AVFormatContext *pFormartContext;
+    AVInputFormat *inputFormart;
+    AVCodecContext *pCodecContext;
+    AVCodecParameters  *pCodecPar;
+    AVCodec *codeC;
+    AVPacket pPackct;
+    AVFrame *pFrame,*pYUVFrame;
+    AVSubtitle subtitle;
+    uint8_t data;
+    int videoStream;
+    CADisplayLink *displayLink;
+    UIImageView *imageView;
+}
+@property(nonatomic,strong)MoviePlayer *player;
 @end
 
 @implementation ViewController
@@ -17,7 +38,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    imageView=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 200)];
+    imageView.image=[UIImage imageNamed:@"main_demo_1"];
+    [self.view addSubview:imageView];
+    [self test2];
+    
 }
+
+//用cadisplaylink
+-(void)test2{
+    NSString *path=[[NSBundle mainBundle]pathForResource:@"sintel" ofType:@"mov"];
+    _player=[[MoviePlayer alloc]initWithVideo:path];
+    int tns, thh, tmm, tss;
+    tns = _player.duration;
+    thh = tns / 3600;
+    tmm = (tns % 3600) / 60;
+    tss = tns % 60;
+    [_player seekTime:0];
+    [NSTimer scheduledTimerWithTimeInterval: 1 / _player.fps
+                                     target:self
+                                   selector:@selector(displayNextFrame:)
+                                   userInfo:nil
+                                    repeats:YES];
+}
+-(void)displayNextFrame:(NSTimer *)timer {
+//    NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+    //    self.TimerLabel.text = [NSString stringWithFormat:@"%f s",video.currentTime];
+    
+    if (![_player stepFrame]) {
+        return;
+    }
+    imageView.image=_player.currentImage;
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
